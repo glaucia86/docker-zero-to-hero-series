89de9d22118ee315dbff986811885b1c4be3b83f
glaucia86 <glaucia_lemos86@hotmail.com>
2019-07-05 19:42:12 -0300

    Criando o arquivo 'Dockerfile' no exemplo-1

---
+++ exemplo-1/app.js
@@ -0,0 +1,23 @@
+/**
+ * Arquivo: app.js
+ * Descrição: arquivo principal e responsável pela execução da aplicação.
+ * Data: 05/07/2019
+ * Author: Glaucia Lemos
+ */
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'Seja Bem-Vindo(a) ao mundo Docker!',
+    version: '1.0.0',
+  });
+});
+
+app.listen(port);
+console.log('Aplicação executando na porta ', port);
